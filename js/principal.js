74cb11b79ca78a7a81aa7b937444e968d0c33286
Natalia Schiroff <natalia.schiroff@escola.pr.gov.br>
2025-10-07 09:33:23 -0300

    yggt

--- js/principal.js
+++ js/principal.js
@@ -6,14 +6,14 @@ titulo.textContent = ("Aparecida Nutricionista");
 var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
 
-var tdNome = document.querySelector(".info-nome");
+var tdNome = paciente.querySelector(".info-nome");
 var nome = tdPeso.textContent;
 console.log(nome);
 
-var tdPeso = document.querySelector(".info-altura");
+var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdAltura.textContent;
 console.log(peso);
 
-var tdAltura = document.querySelector(".info-altura");
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
