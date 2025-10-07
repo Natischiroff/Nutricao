aed5b7f69b2664e8657884afb04381156c44f9a6
Natalia Schiroff <natalia.schiroff@escola.pr.gov.br>
2025-10-07 09:11:36 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -8,11 +8,8 @@ var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
 
 var tdPeso = paciente.querySelector(".info-peso");
-console.log(tdPeso);
-
 var peso = tdPeso.textContent;
-console.log(paciente)
-console.log(peso)
+console.log(peso);
 
 var tdAltura = document.querySelector(".info-altura")
 var altura = tdAltura.textContent;
