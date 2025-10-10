7599c2217bcab36a5f0cc41ebf371117ce233780
Natalia Schiroff <natalia.schiroff@escola.pr.gov.br>
2025-10-10 10:55:18 -0300

    2

--- js/principal.js
+++ js/principal.js
@@ -32,9 +32,8 @@ if (peso < 0 || peso > 1000) {
 if (altura < 0 || altura > 3.00) {
     console.log("Altura inválida");
     alturaEhValida = false;
-    tdImc.textContent = " Altura inválida!"
+    tdImc.textContent = " Altura inválida!";
 }
-
 
 if (alturaEhValida && pesoEhValido) {
     var imc = peso / (altura * altura);
