e2c46e79738f004cf5ce50f103fe27e12eb8ae19
Natalia Schiroff <natalia.schiroff@escola.pr.gov.br>
2025-11-07 11:18:14 -0300

    yjjh

--- js/form.js
+++ js/form.js
@@ -1,13 +1,9 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
-     //alert("Oi eu sou o botao e fui clicado");
+    //alert("Oi eu sou o botao e fui clicado");
     var form = document.querySelector("#form-adiciona");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
 
     var pacienteTr = document.createElement("tr");
 
@@ -21,7 +17,7 @@ botaoAdicionar.addEventListener("click", function (event) {
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
-    imcTd.textContent= calculaImc(peso,altura);
+    imcTd.textContent = calculaImc(peso, altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
@@ -32,4 +28,15 @@ botaoAdicionar.addEventListener("click", function (event) {
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
-});
+
+    function obtemPacienteDoFormulario(form) {
+        var paciente = {
+            nome:form.nome.value;
+            peso:form.peso.value;
+            altura:form.altura.value;
+            gordura:form.gordura.value;
+            imc:calculaImc(form.peso.value,form.altura.value)
+
+
+        }
+    }
